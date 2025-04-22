c8ce946f2e7eee950e8e77fdd99649f7caf0a241
Julia Picole Turubia <106983950+juliaturubia@users.noreply.github.com>
2025-04-22 10:29:05 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char estadoa;
+    // Váriavéis da Primeira Carta
+    char estadoa; 
     char codigodacartaa[10];
     char nomedacidadea[50];
     unsigned long int populacaoa;  
@@ -10,7 +11,7 @@ int main() {
     int pontosturisticosa;
 
 
-
+    // Váriavéis da Segunda Carta
     char estadob;
     char codigodacartab[10];
     char nomedacidadeb[50];
@@ -20,11 +21,12 @@ int main() {
     int pontosturisticosb;
 
 
+    //Entrada da Primeira Carta
     printf("\nDigite os dados da Primeira Carta:\n");
 
-    printf("Estado (letra de A a H): ");
-    scanf(" %c", &estadoa);
-    getchar(); // limpa o Enter
+    printf("Estado (letra de A a H): "); // Pergunta ao Usuário
+    scanf(" %c", &estadoa); // Armazena a Váriavel
+    getchar(); // Limpa o Enter
 
     printf("Código da Carta (ex: A01): ");
     fgets(codigodacartaa, sizeof(codigodacartaa), stdin);
@@ -47,8 +49,8 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontosturisticosa);
 
-    float densidadepopa = populacaoa/areaa;
-    float pibpercapa = (piba * 1000000000) / populacaoa;
+    float densidadepopa = populacaoa/areaa; // Divide a População da Primeira Carta pela Area da Primeira Carta
+    float pibpercapa = (piba * 1000000000) / populacaoa; // Multiplica o PIB da Primeira Carta por 1000000000 e Divide pela População da Primeira Carta
 
     getchar();
     printf("Densidade populacional: %.2f hab/km² \n", densidadepopa);
@@ -56,8 +58,7 @@ int main() {
     printf("PIB per capita: %.2f reais\n", pibpercapa);
 
 
-
-
+    //Entrada da Segunda Carta
     printf("\nDigite os dados da Segunda Carta:\n");
 
     printf("Estado (letra de A a H): ");
@@ -86,21 +87,23 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontosturisticosb);
 
-    float densidadepopb = populacaob / areab;
-    float pibpercapb = (pibb * 1000000000) / populacaob;
+    float densidadepopb = populacaob / areab; // Divide a População da Segunda Carta pela Area da Primeira Carta
+    float pibpercapb = (pibb * 1000000000) / populacaob; // Multiplica o PIB da Segunda Carta por 1000000000 e Divide pela População da Primeira Carta
 
     getchar();
     printf("Densidade populacional: %.2f hab/km²\n", densidadepopb);
 
     printf("PIB per capita: %.2f reais\n ", pibpercapb);
     
-    float superpodera = (float)populacaoa + areaa + (piba * 1000000000) + pontosturisticosa + pibpercapa + (1 / densidadepopa);
-    float superpoderb = (float)populacaob + areab + (pibb * 1000000000) + pontosturisticosb + pibpercapb + (1 / densidadepopb);
+    float superpodera = (float)populacaoa + areaa + (piba * 1000000000) + pontosturisticosa + pibpercapa + (1 / densidadepopa);  //Calcula os Resultados das Váriaveis da Primeira Carta para a Comparação
+    float superpoderb = (float)populacaob + areab + (pibb * 1000000000) + pontosturisticosb + pibpercapb + (1 / densidadepopb);  //Calcula os Resultados das Váriaveis da Segunda Carta para a Comparação
 
-    printf("\nComparação de Cartas:\n");
+
+    //Compara os Resultados das Váriaveis da Primeira Carta e da Segunda Carta
+    printf("\nComparação de Cartas:\n");  
 
     printf("População: Carta %d venceu %s\n", populacaoa > populacaob ? 1 : 2, populacaoa > populacaob ? "(1)" : "(2)");
-    printf("Área: Carta %d venceu %s\n", areaa > areab ? 1 : 2, areaa > areab ? "(1)" : "(2)");
+    printf("Área: Carta %d venceu %s\n", areaa > areab ? 1 : 2, areaa > areab ? "(1)" : "(2)"); 
     printf("PIB: Carta %d venceu %s\n", piba > pibb ? 1 : 2, piba > pibb ? "(1)" : "(2)");
     printf("Pontos Turísticos: Carta %d venceu %s\n", pontosturisticosa > pontosturisticosb ? 1 : 2, pontosturisticosa > pontosturisticosb ? "(1)" : "(2)");
     printf("Densidade Populacional: Carta %d venceu %s\n", densidadepopa < densidadepopb ? 1 : 2, densidadepopa < densidadepopb ? "(1)" : "(2)");
